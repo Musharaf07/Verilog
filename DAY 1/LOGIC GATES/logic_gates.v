bb8454c57e7082417d1c2683c8be75513d3fe597
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-02 16:53:00 +0530

    Update logic_gates.v

--- DAY 1/LOGIC GATES/logic_gates.v
+++ DAY 1/LOGIC GATES/logic_gates.v
@@ -1,1 +1,11 @@
-
+// LOGIC GATES
+module logic_gates(input a,b, output y_and, y_or, y_not, y_nand, y_nor, y_xor, y_xnor);
+  assign y_and  = a&b	 ;
+  assign y_or   = a|b	 ;
+  assign y_not  = ~a	 ;
+  assign y_nand = ~(a&b) ; 
+  assign y_nor  = ~(a|b) ;
+  assign y_xor  = a^b	 ;
+  assign y_xnor = ~(a^b) ;
+  
+endmodule

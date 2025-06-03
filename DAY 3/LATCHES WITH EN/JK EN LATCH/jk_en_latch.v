c6d20d383134ab1edac7ce72fe23b088480d1ece
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-03 11:42:54 +0530

    Create jk_en_latch.v

---
+++ DAY 3/LATCHES WITH EN/JK EN LATCH/jk_en_latch.v
@@ -0,0 +1,14 @@
+// JK LATCH WITH ENABLE
+module jk_latch(output reg q, qb, input j,k,en);
+  always@(*)begin
+    if(en) begin
+    case({j,k})
+      2'b00 : q= q ;
+      2'b01 : q= 0 ;
+      2'b10 : q= 1 ;
+      2'b11 : q= ~q;
+    endcase
+    qb = ~q;
+    end
+  end 
+endmodule

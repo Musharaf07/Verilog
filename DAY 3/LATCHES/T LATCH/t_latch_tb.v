91d92367d1edb72fc44a041e22ee9cc91cb7e823
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-03 10:43:05 +0530

    Create t_latch_tb.v

---
+++ DAY 3/LATCHES/T LATCH/t_latch_tb.v
@@ -0,0 +1,21 @@
+// T LATCH TB
+module t_latch_tb();
+  reg t;
+  wire q,qb;
+  
+  t_latch dut(.t(t),.q(q),.qb(qb));
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    $monitor("T=%b Q=%b QB=%b",t,q,qb);
+  end
+  
+  initial begin
+    t=1; #10
+    t=0; #10
+    t=1; #10
+    t=0; #10
+    $finish;
+  end 
+endmodule

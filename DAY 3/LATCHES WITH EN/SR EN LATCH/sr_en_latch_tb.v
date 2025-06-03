d82909128408aa93b11234c3258060f66a24f5af
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-03 11:51:54 +0530

    Create sr_en_latch_tb.v

---
+++ DAY 3/LATCHES WITH EN/SR EN LATCH/sr_en_latch_tb.v
@@ -0,0 +1,22 @@
+// SR LATCH WITH ENABLE TB
+module sr_latch_tb();
+  reg s,r,en;
+  wire q,qb;
+  
+  sr_latch dut(.s(s),.r(r),.q(q),.qb(qb),.en(en));
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    $monitor("en=%b S=%b R=%b Q=%b QB=%b",en,s,r,q,qb);
+  end
+  
+  initial begin
+    en=1; s=1; r=0; #10
+    en=0; s=0; r=0; #10
+    en=1; s=0; r=1; #10
+    en=1; s=0; r=0; #10
+    en=0; s=1; r=1; #10
+    $finish;
+  end 
+endmodule

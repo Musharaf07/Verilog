0e518805f66d41b1b6aa384ae9d7a9e1c1d6a3a7
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-03 10:42:39 +0530

    Create t_latch.v

---
+++ DAY 3/LATCHES/T LATCH/t_latch.v
@@ -0,0 +1,11 @@
+// T LATCH
+module t_latch(output reg q, qb, input t);
+  initial q=0;
+  always@(*)begin
+    case(t)
+      1'b0 : q=q;
+      1'b1 : q=~q;
+    endcase
+    qb = ~q;
+  end 
+endmodule

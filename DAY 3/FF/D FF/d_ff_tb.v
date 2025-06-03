a73ef7140319cd8e8fc07c7defb22c182d5d9756
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-03 15:04:13 +0530

    Create d_ff_tb.v

---
+++ DAY 3/FF/D FF/d_ff_tb.v
@@ -0,0 +1,27 @@
+// D FF TB
+module d_ff_tb();
+  reg d,clk;
+  wire q,qb;
+  
+  initial begin
+    clk = 0;
+  end
+  
+  d_ff dut(.d(d),.q(q),.qb(qb),.clk(clk));
+  
+  always begin
+    #5 clk = ~clk;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    $monitor("clk=%b D=%b Q=%b QB=%b",clk,d,q,qb);
+  end
+  
+  initial begin
+    d=1; #10
+    d=0; #10
+    $finish;
+  end 
+endmodule

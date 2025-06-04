83e9c44c627c6f35ae2d5aa998e687de825f08f4
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-04 12:02:25 +0530

    Create sr_ff_tb.v

---
+++ DAY 3/FF/SR FF/sr_ff_tb.v
@@ -0,0 +1,30 @@
+// SR FF TB
+module sr_ff_tb();
+  reg s,r,clk;
+  wire q,qb;
+  
+  initial begin
+    clk = 0;
+  end
+  
+  sr_ff dut(.s(s),.r(r),.q(q),.qb(qb),.clk(clk));
+  
+  always begin
+    #5 clk = ~clk;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    $monitor("clk=%b S=%b R=%b Q=%b QB=%b",clk,s,r,q,qb);
+  end
+  
+  initial begin
+    s=1; r=0; #10
+    s=0; r=0; #10
+    s=0; r=1; #10
+    s=0; r=0; #10
+    s=1; r=1; #10
+    $finish;
+  end
+endmodule

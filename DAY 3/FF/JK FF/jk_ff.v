25661afefc98dec6383ecb83276deacd975fb8fe
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-04 11:12:18 +0530

    Create jk_ff.v

---
+++ DAY 3/FF/JK FF/jk_ff.v
@@ -0,0 +1,13 @@
+//JK FF
+module jk_ff(input j,k,clk, output reg q, qb);
+  always@(posedge clk)begin
+    
+    case({j,k})
+      2'b00 : q= q;
+      2'b01 : q= 0;
+      2'b10 : q= 1;
+      2'b11 : q=~q;
+    endcase
+    qb = ~q;
+  end
+endmodule

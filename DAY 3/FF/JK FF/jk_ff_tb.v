de2cdd989be36e72a92b29a846d143fb1ca434d0
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-04 11:12:55 +0530

    Create jk_ff_tb.v

---
+++ DAY 3/FF/JK FF/jk_ff_tb.v
@@ -0,0 +1,30 @@
+// JK FF TB
+module jk_ff_tb();
+  reg j,k,clk;
+  wire q,qb;
+  
+  initial begin
+    clk = 0;
+  end
+  
+  jk_ff dut(.j(j),.k(k),.q(q),.qb(qb),.clk(clk));
+  
+  always begin
+    #5 clk = ~clk;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars;
+    $monitor("clk=%b J=%b K=%b Q=%b QB =%b",clk,j,k,q,qb);
+  end
+  
+  initial begin
+    j=1; k=0; #10
+    j=0; k=0; #10
+    j=0; k=1; #10
+    j=0; k=0; #10
+    j=1; k=1; #10
+    $finish;
+  end
+endmodule

d5ce9f267567c43510cf81c0095a2b23da8aaadb
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-09 16:41:34 +0530

    Update siso.v

--- DAY 4/REGISTERS/SISO/siso.v
+++ DAY 4/REGISTERS/SISO/siso.v
@@ -1,1 +1,13 @@
-
+// SISO - Serial-In Serial-Out (Left shift)
+module siso(output reg out, input s, clk, rst);
+  reg [3:0] so;
+  always @(posedge clk) begin
+    if (rst) begin
+      so <= 4'b0000;
+      out <= 1'b0;
+    end else begin
+      so <= {so[2:0], s};
+      out <= so[3];
+    end
+  end
+endmodule

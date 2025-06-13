57c029a6d6c2a509cfe6e59a0201b69b82e362d3
Musharaf07 <146569624+Musharaf07@users.noreply.github.com>
2025-06-13 15:33:54 +0530

    Create Testbench.v

---
+++ ASSIGNMENTS/Test Assignment/1. Number Sequence in Diamond Pattern/Testbench.v
@@ -0,0 +1,8 @@
+//Number Sequence in Diamond Pattern Tb
+module seq_shape_tb;
+  seq_shape duv();
+   initial begin
+     $display("Sequence Diamond shape:");
+     #100;$finish;
+     end
+ endmodule
